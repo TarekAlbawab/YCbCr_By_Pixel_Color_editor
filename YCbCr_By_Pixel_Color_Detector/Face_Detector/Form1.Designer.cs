2fdeabe77a499bd9c583358c1aaebefdf81f1b3e
Tarek <tarek4u24@gmail.com>
2017-07-16 07:20:21 +0300

    Add files via upload

---
+++ YCbCr_By_Pixel_Color_Detector/Face_Detector/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace Face_Detector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxInput = new System.Windows.Forms.PictureBox();
+            this.pictureBoxOutput = new System.Windows.Forms.PictureBox();
+            this.insert_button = new System.Windows.Forms.Button();
+            this.process_button = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxInput
+            // 
+            this.pictureBoxInput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxInput.Location = new System.Drawing.Point(52, 45);
+            this.pictureBoxInput.Name = "pictureBoxInput";
+            this.pictureBoxInput.Size = new System.Drawing.Size(388, 351);
+            this.pictureBoxInput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxInput.TabIndex = 0;
+            this.pictureBoxInput.TabStop = false;
+            // 
+            // pictureBoxOutput
+            // 
+            this.pictureBoxOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxOutput.Location = new System.Drawing.Point(473, 45);
+            this.pictureBoxOutput.Name = "pictureBoxOutput";
+            this.pictureBoxOutput.Size = new System.Drawing.Size(388, 351);
+            this.pictureBoxOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxOutput.TabIndex = 1;
+            this.pictureBoxOutput.TabStop = false;
+            // 
+            // insert_button
+            // 
+            this.insert_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.insert_button.Location = new System.Drawing.Point(103, 419);
+            this.insert_button.Name = "insert_button";
+            this.insert_button.Size = new System.Drawing.Size(266, 49);
+            this.insert_button.TabIndex = 2;
+            this.insert_button.Text = "INSERT";
+            this.insert_button.UseVisualStyleBackColor = true;
+            this.insert_button.Click += new System.EventHandler(this.insert_button_Click);
+            // 
+            // process_button
+            // 
+            this.process_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.process_button.Location = new System.Drawing.Point(536, 419);
+            this.process_button.Name = "process_button";
+            this.process_button.Size = new System.Drawing.Size(266, 49);
+            this.process_button.TabIndex = 3;
+            this.process_button.Text = "PROCESS";
+            this.process_button.UseVisualStyleBackColor = true;
+            this.process_button.Click += new System.EventHandler(this.process_button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(178, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(127, 39);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(599, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 39);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Modified";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ClientSize = new System.Drawing.Size(921, 489);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.process_button);
+            this.Controls.Add(this.insert_button);
+            this.Controls.Add(this.pictureBoxOutput);
+            this.Controls.Add(this.pictureBoxInput);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxInput;
+        private System.Windows.Forms.PictureBox pictureBoxOutput;
+        private System.Windows.Forms.Button insert_button;
+        private System.Windows.Forms.Button process_button;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ YCbCr_By_Pixel_Color_Detector/Face_Detector/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using AForge.Imaging.Filters;
+
+namespace Face_Detector
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap _inputImage; //make a global variable to be accessable to all the loops
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void insert_button_Click(object sender, EventArgs e)
+        {
+            var openfileDialog = new OpenFileDialog();
+            if (openfileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _inputImage = (Bitmap)System.Drawing.Image.FromFile(openfileDialog.FileName);
+                pictureBoxInput.Image = _inputImage;
+
+            }
+        }
+
+        private void process_button_Click(object sender, EventArgs e)
+        {
+            //changes are made here
+
+            ////////YCbCr COLOUR PIXEL FILTERING
+
+            var yCbCrFilter = new YCbCrFiltering();
+            yCbCrFilter.Cb = new AForge.Range(-0.6f, 0);//horizon //segment the red colour
+            yCbCrFilter.Cr = new AForge.Range(0, 0.2f);//vertical
+            var yCbCrFilterOuptut = yCbCrFilter.Apply(_inputImage);
+            pictureBoxOutput.Image = yCbCrFilterOuptut;
+        }
+    }
+}
